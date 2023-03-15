d9ed618bc5c09dfdbd9d85d856f76f5a5f314c99
TimM32 <timmaupin@live.com>
2023-03-14 19:34:54 -0500

    add in answers to prompts

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 'use strict';
 console.log('app file is connected!');
+
 let userName = prompt('What is your name?');
 console.log('user name is: ', userName);
 
@@ -10,59 +11,55 @@ let lowerCaseInput = userAnswer.toLowerCase();
 console.log('🚀 ~ file: app.js:20 ~ lowerCaseInput:', lowerCaseInput);
 
 if (lowerCaseInput === 'yes' || lowerCaseInput === 'y') {
-  alert('Your right! Video games help me relax!');
+    alert('Your right! Video games help me relax!');
 } else {
-  alert('Wrong answer! I do play video games in my spare time');
+    alert('Wrong answer! I do play video games in my spare time');
 }
 
 let userAnswer = prompt('Did I play sports in College? Please answer yes or no!');
 console.log('🚀 ~ file: app.js:16 ~ userAnswer:', userAnswer);
 
-// transform to lower Case
 let lowerCaseInput = userAnswer.toLowerCase();
 console.log('🚀 ~ file: app.js:20 ~ lowerCaseInput:', lowerCaseInput);
 
 if (lowerCaseInput === 'yes' || lowerCaseInput === 'y') {
-  alert('Correct! Played Rugby at SMC!');
+    alert('Correct! Played Rugby at SMC!');
 } else {
-  alert('Incorrect! Sports was a vital part of my college experience');
+    alert('Incorrect! Sports was a vital part of my college experience');
 }
 
 let userAnswer = prompt('Have I lived outside the U.S.A? Please answer yes or no!');
 console.log('🚀 ~ file: app.js:16 ~ userAnswer:', userAnswer);
 
-// transform to lower Case
 let lowerCaseInput = userAnswer.toLowerCase();
 console.log('🚀 ~ file: app.js:20 ~ lowerCaseInput:', lowerCaseInput);
 
 if (lowerCaseInput === 'yes' || lowerCaseInput === 'y') {
-  alert('Your right! I lived in Ireland for 4 years!');
+    alert('Your right! I lived in Ireland for 4 years!');
 } else {
-  alert('Sorry, you are wrong!');
+    alert('Sorry, you are wrong!');
+}
+
+let userAnswer = prompt('Is Cake my favorite dessert? Please answer yes or no!');
+console.log('🚀 ~ file: app.js:16 ~ userAnswer:', userAnswer);
+
+let lowerCaseInput = userAnswer.toLowerCase();
+console.log('🚀 ~ file: app.js:20 ~ lowerCaseInput:', lowerCaseInput);
+
+if (lowerCaseInput === 'yes' || lowerCaseInput === 'y') {
+    alert('You are unfortunately Wrong! Cookies would be number 1!');
+} else {
+    alert('Right answer! Homemade Cookies are my favorite, but cant is a close second!');
 }
 
 let userAnswer = prompt('Is my favorite food pizza? Please answer yes or no!');
 console.log('🚀 ~ file: app.js:16 ~ userAnswer:', userAnswer);
 
-// transform to lower Case
 let lowerCaseInput = userAnswer.toLowerCase();
 console.log('🚀 ~ file: app.js:20 ~ lowerCaseInput:', lowerCaseInput);
 
 if (lowerCaseInput === 'yes' || lowerCaseInput === 'y') {
-  alert('Your right! My favorite food is pizza!');
+    alert('Your right! My favorite food is pizza!');
 } else {
-  alert('Nope! Pizza is my favorite food!');
-}
-
-let userAnswer = prompt('Is my favorite food pizza? Please answer yes or no!');
-console.log('🚀 ~ file: app.js:16 ~ userAnswer:', userAnswer);
-
-// transform to lower Case
-let lowerCaseInput = userAnswer.toLowerCase();
-console.log('🚀 ~ file: app.js:20 ~ lowerCaseInput:', lowerCaseInput);
-
-if (lowerCaseInput === 'yes' || lowerCaseInput === 'y') {
-  alert('Your right! My favorite food is pizza!');
-} else {
-  alert('Nope! Pizza is my favorite food!');
+    alert('Nope! Pizza is my favorite food!');
 }

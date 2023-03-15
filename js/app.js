b0ed7f20a1297b138178b6896cf0a0e98ad526e0
TimM32 <timmaupin@live.com>
2023-03-14 19:18:15 -0500

    add the 4 other prompts

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 'use strict';
 console.log('app file is connected!');
+let userName = prompt('What is your name?');
+console.log('user name is: ', userName);
+
 let userAnswer = prompt('Do I sometimes play video games in my free time? Please answer yes or no!');
 console.log('🚀 ~ file: app.js:16 ~ userAnswer:', userAnswer);
 
@@ -10,4 +13,56 @@ if (lowerCaseInput === 'yes' || lowerCaseInput === 'y') {
   alert('Your right! Video games help me relax!');
 } else {
   alert('Wrong answer! I do play video games in my spare time');
+}
+
+let userAnswer = prompt('Did I play sports in College? Please answer yes or no!');
+console.log('🚀 ~ file: app.js:16 ~ userAnswer:', userAnswer);
+
+// transform to lower Case
+let lowerCaseInput = userAnswer.toLowerCase();
+console.log('🚀 ~ file: app.js:20 ~ lowerCaseInput:', lowerCaseInput);
+
+if (lowerCaseInput === 'yes' || lowerCaseInput === 'y') {
+  alert('Correct! Played Rugby at SMC!');
+} else {
+  alert('Incorrect! Sports was a vital part of my college experience');
+}
+
+let userAnswer = prompt('Is my favorite food pizza? Please answer yes or no!');
+console.log('🚀 ~ file: app.js:16 ~ userAnswer:', userAnswer);
+
+// transform to lower Case
+let lowerCaseInput = userAnswer.toLowerCase();
+console.log('🚀 ~ file: app.js:20 ~ lowerCaseInput:', lowerCaseInput);
+
+if (lowerCaseInput === 'yes' || lowerCaseInput === 'y') {
+  alert('Your right! My favorite food is pizza!');
+} else {
+  alert('Nope! Pizza is my favorite food!');
+}
+
+let userAnswer = prompt('Is my favorite food pizza? Please answer yes or no!');
+console.log('🚀 ~ file: app.js:16 ~ userAnswer:', userAnswer);
+
+// transform to lower Case
+let lowerCaseInput = userAnswer.toLowerCase();
+console.log('🚀 ~ file: app.js:20 ~ lowerCaseInput:', lowerCaseInput);
+
+if (lowerCaseInput === 'yes' || lowerCaseInput === 'y') {
+  alert('Your right! My favorite food is pizza!');
+} else {
+  alert('Nope! Pizza is my favorite food!');
+}
+
+let userAnswer = prompt('Is my favorite food pizza? Please answer yes or no!');
+console.log('🚀 ~ file: app.js:16 ~ userAnswer:', userAnswer);
+
+// transform to lower Case
+let lowerCaseInput = userAnswer.toLowerCase();
+console.log('🚀 ~ file: app.js:20 ~ lowerCaseInput:', lowerCaseInput);
+
+if (lowerCaseInput === 'yes' || lowerCaseInput === 'y') {
+  alert('Your right! My favorite food is pizza!');
+} else {
+  alert('Nope! Pizza is my favorite food!');
 }

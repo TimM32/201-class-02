e13ef247337084726e1b7317c9ba107d38b2570e
TimM32 <timmaupin@live.com>
2023-03-16 14:01:09 -0500

    add getVideoGames function

--- js/app.js
+++ js/app.js
@@ -10,25 +10,20 @@ let userName = prompt('What is your name?');
 console.log('user name is: ', userName);
 alert('Welcome ' + userName);
 
-
-
-
-
+function getVideoGames(); {
 let videoGames = prompt('Do I sometimes play video games in my free time? Please answer yes or no!');
-
 let gamelowerCase = videoGames.toLowerCase();
-
 if (gamelowerCase === 'yes' || gamelowerCase === 'y') {
   alert('Your right! Video games help me relax!');
   userPoints = userPoints + 1;
 } else {
   alert('Wrong answer! I do play video games in my spare time');
 }
+}
+
 
 let sportsAnswer = prompt('Did I play sports in College? Please answer yes or no!');
-
 let sportlowerCase = sportsAnswer.toLowerCase();
-
 if (sportlowerCase === 'yes' || sportlowerCase === 'y') {
   alert('Correct! Played Rugby at SMC!');
   userPoints = userPoints + 1;
@@ -36,8 +31,8 @@ if (sportlowerCase === 'yes' || sportlowerCase === 'y') {
   alert('Incorrect! Sports was a vital part of my college experience');
 }
 
-let countryAnswer = prompt('Have I lived outside the U.S.A? Please answer yes or no!');
 
+let countryAnswer = prompt('Have I lived outside the U.S.A? Please answer yes or no!');
 let irelandlowerCase = countryAnswer.toLowerCase();
 if (irelandlowerCase === 'yes' || irelandlowerCase === 'y') {
   alert('Your right! I lived in Ireland for 4 years!');
@@ -46,10 +41,10 @@ if (irelandlowerCase === 'yes' || irelandlowerCase === 'y') {
   alert('Sorry, you are wrong!');
 }
 
+
+
 let dessertAnswer = prompt('Is Cake my favorite dessert? Please answer yes or no!');
-
 let dessertlowerCase = dessertAnswer.toLowerCase();
-
 if (dessertlowerCase === 'yes' || dessertlowerCase === 'y') {
   alert('You are unfortunately Wrong! Cookies would be number 1!');
 } else {
@@ -57,10 +52,10 @@ if (dessertlowerCase === 'yes' || dessertlowerCase === 'y') {
   userPoints = userPoints + 1;
 }
 
+
+
 let favSports = prompt('Is Football my favorite Sport? Please answer yes or no!');
-
 let favlowerCase = favSports.toLowerCase();
-
 if (favlowerCase === 'yes' || favlowerCase === 'y') {
   alert('Nope! That would be Rugby, I played it!');
 } else {
@@ -115,4 +110,4 @@ alert('Thanks for playing my game ' + userName);
 let results = document.getElementById('results');
 console.log(results);
 let finalresults = 'You guessed' + userAttempts + ' and got' + correctAnswer + ' correct answers.';
-results.innerHTML= finalresults;
+results.innerHTML = finalresults;
